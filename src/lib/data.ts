016ae64755e214e6ac5a28b926c10731a0584bd5
Donají Ramos <donajiramosn@gmail.com>
2025-05-21 14:21:00 -0600

    more projects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -113,7 +113,41 @@ export const projects = [
       "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
       "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
     ],
+    
+
+
+
   },
+  {
+    title: "Power BI",
+    github: "https://github.com/rishikesh2003/mental-aarog",
+    description: [
+      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+  
+  },
+  {
+    title: "R proyecto",
+    github: "https://github.com/rishikesh2003/mental-aarog",
+    description: [
+      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+
+  }
+
+  
+  
+
+  
+
 ];
 
 export const awards = [
